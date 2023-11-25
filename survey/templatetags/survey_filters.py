183dab15fbce2a4fe5962bb6ed8c44f79a1993b3
Katherine Zambrano <kzambrano@vigigo.com>
2023-11-24 20:04:13 -0500

    Conteo de resp, likes y dislikes

--- survey/templatetags/survey_filters.py
+++ survey/templatetags/survey_filters.py
@@ -17,3 +17,16 @@ def check_user_answer(question, user):
         return None
     answer = Answer.objects.filter(question=question, author=user).first()
     return str(answer.value) if answer else None
+
+
+@register.filter(name='count_answers')
+def count_answers(question):
+    return question.answers.count()
+
+@register.filter(name='count_likes')
+def count_answers(question):
+    return question.like
+
+@register.filter(name='count_dislikes')
+def count_answers(question):
+    return question.dislike
